37112886c290d92817eeb119728fa922d053b462
Ahmet Can Ogreten <ahmetcan@lidyagenomics.com>
2025-03-10 17:18:04 +0300

    chore: version incremented to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lidya_file_access_repository",
-    version="0.1.0",
+    version="0.2.0",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
